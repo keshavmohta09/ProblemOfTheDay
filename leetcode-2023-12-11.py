f22baf510a1eccb3d45560db12d173f2310bda1a
Keshav Mohta <keshavmohta09@gmail.com>
2023-12-11 14:35:51 +0530

    add 2nd approach

--- leetcode-2023-12-11.py
+++ leetcode-2023-12-11.py
@@ -6,21 +6,39 @@ from typing import List
 
 class Solution:
     def findSpecialInteger(self, arr: List[int]) -> int:
-        max_prev = arr[0]
-        max_prev_freq = 0
+        # Solution 1 ->
+        # max_prev = arr[0]
+        # max_prev_freq = 0
 
+        # prev = arr[0]
+        # prev_freq = 0
+
+        # for i in arr:
+        #     if i==prev:
+        #         prev_freq+=1
+        #     else:
+        #         prev = i
+        #         prev_freq = 1
+
+        #     if prev_freq>max_prev_freq:
+        #         max_prev = prev
+        #         max_prev_freq = prev_freq
+
+        # return max_prev
+
+        # Solution 2 ->
+        qtr = len(arr) // 4
+        count = 0
         prev = arr[0]
-        prev_freq = 0
 
         for i in arr:
             if i == prev:
-                prev_freq += 1
+                count += 1
             else:
                 prev = i
-                prev_freq = 1
+                count = 1
 
-            if prev_freq > max_prev_freq:
-                max_prev = prev
-                max_prev_freq = prev_freq
+            if count > qtr:
+                return prev
 
-        return max_prev
+        return prev
