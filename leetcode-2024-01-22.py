a5715d0794ebbf479a4310af2a39c27bfebb18d5
Keshav Mohta <keshavmohta09@gmail.com>
2024-01-22 23:16:10 +0530

    leetcode problem of the day 22/01/2024

--- leetcode-2024-01-22.py
+++ leetcode-2024-01-22.py
@@ -1,23 +1,24 @@
 """
-Question Link: https://leetcode.com/problems/house-robber/description/?envType=daily-question&envId=2024-01-21
+Question Link: https://leetcode.com/problems/set-mismatch/description/?envType=daily-question&envId=2024-01-22
 """
-
 from typing import List
 
 
 class Solution:
-    def get_answer(self, nums, dp, ind=0):
-        if len(nums) <= ind:
-            return 0
-        if dp[ind] == -1:
-            dp[ind] = max(
-                (nums[ind] + self.get_answer(nums, dp, ind + 2)),
-                self.get_answer(nums, dp, ind + 1),
-            )
-        return dp[ind]
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        arr = [0] * len(nums)
 
-    def rob(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        dp = [-1] * len(nums)
-        return self.get_answer(nums, dp=dp)
+        twice_num = -1
+        missing_num = -1
+
+        for i in nums:
+            if arr[i - 1] == 0:
+                arr[i - 1] = 1
+            else:
+                twice_num = i
+
+        for i in range(len(arr)):
+            if arr[i] == 0:
+                missing_num = i + 1
+                break
+        return [twice_num, missing_num]
